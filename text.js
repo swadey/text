6c22b52362f578202c346d22753511ec378b988d
Wade Shen <swadey@mit.edu>
2017-07-03 10:29:55 -0400

    - optional

--- text.js
+++ text.js
@@ -24,7 +24,7 @@ const default_space   = XRE('[\\s\\p{Zs}]+', 'g');
 // -------------------------------------------------------------------------------------------------------------------------
 // Tokenizers
 // -------------------------------------------------------------------------------------------------------------------------
-function twenglish_cleaner(tw, urls = true, hashtags = true, mentions = true) {
+function twenglish_cleaner(tw, { urls = true, hashtags = true, mentions = true } = {}) {
   let ctw = tw.normalize('NFKC').replace(default_space, " ");
   ctw = urls ? ctw.replace(url_pattern, "\u0030\u20E3") : ctw;
 
